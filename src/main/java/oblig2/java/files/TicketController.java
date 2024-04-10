64e5e8c2d4ae69ba8d5153e7a2cc6ec0411157bf
MikaelSommervold <misom1551@oslomet.no>
2024-04-10 22:37:20 +0200

    Adding sql
    Adding sql and changing code, creating repository

--- src/main/java/oblig2/java/files/TicketController.java
+++ src/main/java/oblig2/java/files/TicketController.java
@@ -1,29 +1,45 @@
 package oblig2.java.files;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class TicketController {
 
-    public final List<Ticket> ticketList = new ArrayList<>();
+    @Autowired
+    TicketRepository rep;
 
     @PostMapping("/lagre")
-    public void lagreBillett(Ticket ticket){
-        ticketList.add(ticket);
+    public void lagreBillett(Ticket ticket, HttpServletResponse res) throws IOException {
+        boolean ok = rep.lagreBillett(ticket);
+        if (!ok) {
+            res.sendError(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Det skjedde noe feil, prøv igjen senere");
+        }
     }
 
     @GetMapping("/hentAlle")
-    public List<Ticket> hentAlle(){
-        return ticketList;
+    public List<Ticket> hentAlle(HttpServletResponse res) throws IOException {
+        List<Ticket> tickets = rep.hentAlle();
+        if (tickets == null) {
+            res.sendError(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Det skjedde noe feil, prøv igjen senere");
+        }
+        return tickets;
     }
-
     @GetMapping("/slettAlle")
-    public void slettAlle(){
-        ticketList.clear();
+    public void slettAlle(HttpServletResponse res) throws IOException {
+        boolean ok = rep.slettAlle();
+        if (!ok){
+            res.sendError(HttpStatus.INTERNAL_SERVER_ERROR.value(),"Feil i Database -prøv igjen senere");
+        }
+    }
+        }
     }
 }
